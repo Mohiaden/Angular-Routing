d64946f83621eec34d0c0b40a5722d2792fad5f8
Mohiaden Mohamoud Mohamed <muxudiinkeey@outlook.com>
2022-03-08 13:48:20 +0100

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductListComponent } from './products/product-list.component';
+import { DetailesComponent } from './products/detailes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: 'products:id', component: DetailesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
